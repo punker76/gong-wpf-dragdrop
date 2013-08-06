0171a75a6336715a7de4f333cb0f133ddc64c1c2
punker76 <punkerat76@gmail.com>
2013-08-07 01:25:53 +0200

    fix starting net35 and net4 example

--- Examples/DefaultsExample/App.xaml.cs
+++ Examples/DefaultsExample/App.xaml.cs
@@ -13,7 +13,11 @@ namespace NorthwindExample
     public partial class App : Application
     {
         public App() {
+#if NET35
             this.StartupUri = new Uri("Window(NET35).xaml", UriKind.RelativeOrAbsolute);
+#else
+            this.StartupUri = new Uri("Window(NET4).xaml", UriKind.RelativeOrAbsolute);
+#endif
         }
     }
 }
