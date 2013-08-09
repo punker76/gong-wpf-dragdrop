5122ec58903a1cae1f1b97e3626686f5a593dbf4
punker76 <punkerat76@gmail.com>
2013-08-09 13:57:30 +0200

    + HitTestUtilities

--- GongSolutions.Wpf.DragDrop/DragDrop.cs
+++ GongSolutions.Wpf.DragDrop/DragDrop.cs
@@ -10,11 +10,6 @@ using GongSolutions.Wpf.DragDrop.Icons;
 using GongSolutions.Wpf.DragDrop.Utilities;
 using System.Windows.Media.Imaging;
 
-#if NET35
-using Microsoft.Windows.Controls;
-using Microsoft.Windows.Controls.Primitives;
-#endif
-
 namespace GongSolutions.Wpf.DragDrop
 {
   public static class DragDrop
@@ -527,55 +522,6 @@ namespace GongSolutions.Wpf.DragDrop
       }
     }
 
-    private static T GetHitTestElement4Type<T>(object sender, Point elementPosition) where T : UIElement
-    {
-      var hit = VisualTreeHelper.HitTest((Visual)sender, elementPosition);
-      if (hit == null) {
-        return null;
-      } else {
-        var uiElement = hit.VisualHit.GetVisualAncestor<T>();
-        return uiElement;
-      }
-    }
-
-    private static bool HitTest4Type<T>(object sender, Point elementPosition) where T : UIElement
-    {
-      var uiElement = GetHitTestElement4Type<T>(sender, elementPosition);
-      return uiElement != null && uiElement.Visibility == Visibility.Visible;
-    }
-
-    private static bool HitTest4GridViewColumnHeader(object sender, Point elementPosition)
-    {
-      if (sender is ListView) {
-        // no drag&drop for column header
-        var columnHeader = GetHitTestElement4Type<GridViewColumnHeader>(sender, elementPosition);
-        if (columnHeader != null && (columnHeader.Role == GridViewColumnHeaderRole.Floating || columnHeader.Visibility == Visibility.Visible)) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    private static bool HitTest4DataGridTypes(object sender, Point elementPosition)
-    {
-      if (sender is DataGrid) {
-        // no drag&drop for column header
-        var columnHeader = GetHitTestElement4Type<DataGridColumnHeader>(sender, elementPosition);
-        if (columnHeader != null && columnHeader.Visibility == Visibility.Visible) {
-          return true;
-        }
-        // no drag&drop for row header
-        var rowHeader = GetHitTestElement4Type<DataGridRowHeader>(sender, elementPosition);
-        if (rowHeader != null && rowHeader.Visibility == Visibility.Visible) {
-          return true;
-        }
-        // drag&drop only for data grid row
-        var dataRow = GetHitTestElement4Type<DataGridRow>(sender, elementPosition);
-        return dataRow == null;
-      }
-      return false;
-    }
-
     private static void Scroll(DependencyObject o, DragEventArgs e)
     {
       var scrollViewer = o.GetVisualDescendent<ScrollViewer>();
@@ -603,12 +549,12 @@ namespace GongSolutions.Wpf.DragDrop
       // Ignore the click if clickCount != 1 or the user has clicked on a scrollbar.
       var elementPosition = e.GetPosition((IInputElement)sender);
       if (e.ClickCount != 1
-          || HitTest4Type<ScrollBar>(sender, elementPosition)
-          || HitTest4Type<TextBoxBase>(sender, elementPosition)
-          || HitTest4Type<PasswordBox>(sender, elementPosition)
-          || HitTest4Type<Slider>(sender, elementPosition)
-          || HitTest4GridViewColumnHeader(sender, elementPosition)
-          || HitTest4DataGridTypes(sender, elementPosition)
+          || HitTestUtilities.HitTest4Type<ScrollBar>(sender, elementPosition)
+          || HitTestUtilities.HitTest4Type<TextBoxBase>(sender, elementPosition)
+          || HitTestUtilities.HitTest4Type<PasswordBox>(sender, elementPosition)
+          || HitTestUtilities.HitTest4Type<Slider>(sender, elementPosition)
+          || HitTestUtilities.HitTest4GridViewColumnHeader(sender, elementPosition)
+          || HitTestUtilities.HitTest4DataGridTypes(sender, elementPosition)
           || GetDragSourceIgnore((UIElement)sender)) {
         m_DragInfo = null;
         return;
@@ -725,9 +671,9 @@ namespace GongSolutions.Wpf.DragDrop
     private static void DropTarget_PreviewDragOver(object sender, DragEventArgs e)
     {
       var elementPosition = e.GetPosition((IInputElement)sender);
-      if (HitTest4Type<ScrollBar>(sender, elementPosition)
-          || HitTest4GridViewColumnHeader(sender, elementPosition)
-          || HitTest4DataGridTypes(sender, elementPosition)) {
+      if (HitTestUtilities.HitTest4Type<ScrollBar>(sender, elementPosition)
+          || HitTestUtilities.HitTest4GridViewColumnHeader(sender, elementPosition)
+          || HitTestUtilities.HitTest4DataGridTypes(sender, elementPosition)) {
         e.Effects = DragDropEffects.None;
         e.Handled = true;
         return;
---
+++ GongSolutions.Wpf.DragDrop/Utilities/HitTestUtilities.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+#if NET35
+using Microsoft.Windows.Controls;
+using Microsoft.Windows.Controls.Primitives;
+#else
+using System.Windows.Controls.Primitives;
+#endif
+
+namespace GongSolutions.Wpf.DragDrop.Utilities
+{
+  public static class HitTestUtilities
+  {
+    public static bool HitTest4Type<T>(object sender, Point elementPosition) where T : UIElement {
+      var uiElement = GetHitTestElement4Type<T>(sender, elementPosition);
+      return uiElement != null && uiElement.Visibility == Visibility.Visible;
+    }
+
+    private static T GetHitTestElement4Type<T>(object sender, Point elementPosition) where T : UIElement
+    {
+      var hit = VisualTreeHelper.HitTest((Visual)sender, elementPosition);
+      if (hit == null) {
+        return null;
+      } else {
+        var uiElement = hit.VisualHit.GetVisualAncestor<T>();
+        return uiElement;
+      }
+    }
+
+    public static bool HitTest4GridViewColumnHeader(object sender, Point elementPosition)
+    {
+      if (sender is ListView) {
+        // no drag&drop for column header
+        var columnHeader = GetHitTestElement4Type<GridViewColumnHeader>(sender, elementPosition);
+        if (columnHeader != null && (columnHeader.Role == GridViewColumnHeaderRole.Floating || columnHeader.Visibility == Visibility.Visible)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public static bool HitTest4DataGridTypes(object sender, Point elementPosition)
+    {
+      if (sender is DataGrid) {
+        // no drag&drop for column header
+        var columnHeader = GetHitTestElement4Type<DataGridColumnHeader>(sender, elementPosition);
+        if (columnHeader != null && columnHeader.Visibility == Visibility.Visible) {
+          return true;
+        }
+        // no drag&drop for row header
+        var rowHeader = GetHitTestElement4Type<DataGridRowHeader>(sender, elementPosition);
+        if (rowHeader != null && rowHeader.Visibility == Visibility.Visible) {
+          return true;
+        }
+        // drag&drop only for data grid row
+        var dataRow = GetHitTestElement4Type<DataGridRow>(sender, elementPosition);
+        return dataRow == null;
+      }
+      return false;
+    }
+  }
+}
