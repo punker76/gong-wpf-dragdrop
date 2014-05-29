39cae4a679085f897e03dc681057ab2193e460a7
Jan Karger <punkerat76@gmail.com>
2014-05-30 01:54:06 +0200

    VS120COMNTOOLS

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,5 +1,5 @@
 @echo on
-call "%VS100COMNTOOLS%vsvars32.bat"
+call "%VS120COMNTOOLS%vsvars32.bat"
 
 msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET35).sln" /p:StrongName=True /p:configuration=Release
 msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET4).sln" /p:StrongName=True /p:configuration=Release
