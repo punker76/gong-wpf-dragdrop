f37f1aa9bc65b45fd07b7d0a8a70f71989897a81
Jan Karger <punkerat76@gmail.com>
2015-03-05 23:00:48 +0100

    ICloneable support for copy or link effect

--- GongSolutions.Wpf.DragDrop/DefaultDropHandler.cs
+++ GongSolutions.Wpf.DragDrop/DefaultDropHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,24 +9,44 @@ using System.Windows.Controls;
 
 namespace GongSolutions.Wpf.DragDrop
 {
+  /// <summary>
+  /// A default insertion drop handler for the most common usages
+  /// </summary>
   public class DefaultDropHandler : IDropTarget
   {
+    /// <summary>
+    /// Updates the current drag state.
+    /// </summary>
+    /// <param name="dropInfo">Information about the drag.</param>
+    /// <remarks>
+    /// To allow a drop at the current drag position, the <see cref="DropInfo.Effects" /> property on
+    /// <paramref name="dropInfo" /> should be set to a value other than <see cref="DragDropEffects.None" />
+    /// and <see cref="DropInfo.Data" /> should be set to a non-null value.
+    /// </remarks>
     public virtual void DragOver(IDropInfo dropInfo)
     {
       if (CanAcceptData(dropInfo)) {
-        dropInfo.Effects = DragDropEffects.Copy;
+        dropInfo.Effects = dropInfo.DragInfo.VisualSource == dropInfo.VisualTarget ? DragDropEffects.Move : DragDropEffects.Copy;
         dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
       }
     }
 
+    /// <summary>
+    /// Performs a drop.
+    /// </summary>
+    /// <param name="dropInfo">Information about the drop.</param>
     public virtual void Drop(IDropInfo dropInfo)
     {
+      if (dropInfo == null || dropInfo.DragInfo == null) {
+        return;
+      }
+      
       var insertIndex = dropInfo.InsertIndex != dropInfo.UnfilteredInsertIndex ? dropInfo.UnfilteredInsertIndex : dropInfo.InsertIndex;
-      var destinationList = dropInfo.TargetCollection.ToList();
+      var destinationList = dropInfo.TargetCollection.TryGetList();
       var data = ExtractData(dropInfo.Data);
 
       if (dropInfo.DragInfo.VisualSource == dropInfo.VisualTarget) {
-        var sourceList = dropInfo.DragInfo.SourceCollection.ToList();
+        var sourceList = dropInfo.DragInfo.SourceCollection.TryGetList();
 
         foreach (var o in data) {
           var index = sourceList.IndexOf(o);
@@ -40,24 +61,35 @@ namespace GongSolutions.Wpf.DragDrop
         }
       }
 
+      var cloneData = dropInfo.Effects.HasFlag(DragDropEffects.Copy) || dropInfo.Effects.HasFlag(DragDropEffects.Link);
       foreach (var o in data) {
-        destinationList.Insert(insertIndex++, o);
+        var obj2Insert = o;
+        if (cloneData) {
+          var cloneable = o as ICloneable;
+          if (cloneable != null) {
+            obj2Insert = cloneable.Clone();
+          }
+        }
+        destinationList.Insert(insertIndex++, obj2Insert);
       }
     }
 
+    /// <summary>
+    /// Test the specified drop information for the right data.
+    /// </summary>
+    /// <param name="dropInfo">The drop information.</param>
     public static bool CanAcceptData(IDropInfo dropInfo)
     {
       if (dropInfo == null || dropInfo.DragInfo == null) {
         return false;
       }
 
-      if (!dropInfo.IsSameDragDropContextAsSource)
-      {
-          return false;
+      if (!dropInfo.IsSameDragDropContextAsSource) {
+        return false;
       }
 
       if (dropInfo.DragInfo.SourceCollection == dropInfo.TargetCollection) {
-        return dropInfo.TargetCollection.ToList() != null;
+        return dropInfo.TargetCollection.TryGetList() != null;
       } else if (dropInfo.DragInfo.SourceCollection is ItemCollection) {
         return false;
       } else if (dropInfo.TargetCollection == null) {
