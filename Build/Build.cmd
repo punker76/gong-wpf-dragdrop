a604d2b5215ceda9ad6f640e0ea76847f10e5521
Jan Karger <punkerat76@gmail.com>
2015-04-01 11:09:20 +0200

    build stuff for alpha/release versions

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -3,6 +3,10 @@ call "%VS120COMNTOOLS%vsvars32.bat"
 
 msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET35).sln" /p:StrongName=True /p:configuration=Release /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET4).sln" /p:StrongName=True /p:configuration=Release /t:Clean,Rebuild
-
 ..\.nuget\NuGet.exe pack %~dp0..\GongSolutions.Wpf.DragDrop\GongSolutions.Wpf.DragDrop.nuspec -OutputDirectory %~dp0
+
+msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET35).sln" /p:StrongName=True /p:configuration=Debug /t:Clean,Rebuild
+msbuild.exe /ToolsVersion:4.0 "..\GongSolutions.Wpf.DragDrop (NET4).sln" /p:StrongName=True /p:configuration=Debug /t:Clean,Rebuild
+..\.nuget\NuGet.exe pack %~dp0..\GongSolutions.Wpf.DragDrop\GongSolutions.Wpf.DragDrop.ALPHA.nuspec -OutputDirectory %~dp0
+
 pause
