57b66e67711f75a20b40697f7f8224a0cd302fb3
Jan Karger <punkerat76@gmail.com>
2015-12-02 11:02:50 +0100

    new version 1.0.0-ALPHA012

--- GongSolutions.Wpf.DragDrop/Properties/AssemblyInfo.cs
+++ GongSolutions.Wpf.DragDrop/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GongSolutions.WPF.DragDrop")]
+[assembly: AssemblyProduct("GongSolutions.WPF.DragDrop")]
 [assembly: AssemblyDescription("A Drag'n'Drop Framework for WPF")]
 [assembly: AssemblyCopyright("Copyright © 2013-2015")]
 
@@ -29,4 +30,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("GongSolutions.WPF.DragDrop 1.0.0-ALPHA011")]
+[assembly: AssemblyInformationalVersion("GongSolutions.WPF.DragDrop 1.0.0-ALPHA012")]
