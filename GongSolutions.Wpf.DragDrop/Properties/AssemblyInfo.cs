351caad5077ec6ac4be00162405a44feaf590844
Jan Karger <punkerat76@gmail.com>
2016-08-05 01:39:07 +0200

    v1.0.0-ALPHA017

--- GongSolutions.Wpf.DragDrop/Properties/AssemblyInfo.cs
+++ GongSolutions.Wpf.DragDrop/Properties/AssemblyInfo.cs
@@ -5,33 +5,18 @@ using System.Windows.Markup;
 [assembly: XmlnsPrefix("urn:gong-wpf-dragdrop", "dd")]
 [assembly: XmlnsDefinition("urn:gong-wpf-dragdrop", "GongSolutions.Wpf.DragDrop")]
 [assembly: XmlnsDefinition("urn:gong-wpf-dragdrop", "GongSolutions.Wpf.DragDrop.Utilities")]
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GongSolutions.WPF.DragDrop")]
-[assembly: AssemblyDescription("A Drag'n'Drop Framework for WPF")]
-[assembly: AssemblyCopyright("Copyright © 2013-2016")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("940084f7-d48e-41b3-9e0d-cf574d587643")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyTitle("GongSolutions.WPF.DragDrop")]
+[assembly: AssemblyDescription("An easy to use drag'n'drop framework for WPF applications.")]
+[assembly: AssemblyCopyright("Copyright © GongSolutions.WPF.DragDrop 2013-2016")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("GongSolutions.WPF.DragDrop 1.0.0-ALPHA016")]
-[assembly: AssemblyProduct("GongSolutions.WPF.DragDrop 1.0.0-ALPHA016")]
+[assembly: AssemblyInformationalVersion("GongSolutions.WPF.DragDrop 1.0.0-ALPHA017")]
+[assembly: AssemblyProduct("GongSolutions.WPF.DragDrop 1.0.0-ALPHA017")]
