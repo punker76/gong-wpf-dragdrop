e29cba20c5f6713b70823ea14d7afc7d3a9d232c
Jan Karger <punkerat76@gmail.com>
2016-08-13 16:11:40 +0200

    raw sample structure

---
+++ src/Showcase.WPF.DragDrop/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Showcase.WPF.DragDrop.ViewModels
+{
+  public class MainViewModel
+  {
+    /// <summary>
+    /// Initializes a new instance of the MainViewModel class.
+    /// </summary>
+    public MainViewModel()
+    {
+      ////if (IsInDesignMode)
+      ////{
+      ////    // Code runs in Blend --> create design time data.
+      ////}
+      ////else
+      ////{
+      ////    // Code runs "for real"
+      ////}
+    }
+  }
+}
