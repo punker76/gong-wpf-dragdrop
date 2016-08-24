5e7f0e1c82a63a078b2515dfb015cdf59cf19859
Jan Karger <punkerat76@gmail.com>
2016-08-24 22:59:48 +0200

    Issue #123 sample

--- src/Showcase.WPF.DragDrop.NET45/ViewModels/MainViewModel.cs
+++ src/Showcase.WPF.DragDrop.NET45/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
-﻿using Showcase.WPF.DragDrop.Models;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Showcase.WPF.DragDrop.Models;
 
 namespace Showcase.WPF.DragDrop.ViewModels
 {
   public class MainViewModel : ViewModelBase
   {
     private SampleData _data;
+    private ICommand _openIssueCommand;
 
     /// <summary>
     /// Initializes a new instance of the MainViewModel class.
@@ -21,6 +24,7 @@ namespace Showcase.WPF.DragDrop.ViewModels
       ////}
 
       this.Data = new SampleData();
+      this.OpenIssueCommand = new SimpleCommand(issue => { Process.Start($"https://github.com/punker76/gong-wpf-dragdrop/issues/{issue}"); });
     }
 
     public SampleData Data
@@ -30,6 +34,17 @@ namespace Showcase.WPF.DragDrop.ViewModels
       {
         if (Equals(value, _data)) return;
         _data = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public ICommand OpenIssueCommand
+    {
+      get { return _openIssueCommand; }
+      set
+      {
+        if (Equals(value, _openIssueCommand)) return;
+        _openIssueCommand = value;
         OnPropertyChanged();
       }
     }
---
+++ src/Showcase.WPF.DragDrop.NET45/ViewModels/SimpleCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Showcase.WPF.DragDrop.ViewModels
+{
+  public class SimpleCommand : ICommand
+  {
+    public SimpleCommand(Action<object> execute = null, Predicate<object> canExecute = null)
+    {
+      this.CanExecuteDelegate = canExecute;
+      this.ExecuteDelegate = execute;
+    }
+
+    public Predicate<object> CanExecuteDelegate { get; set; }
+    public Action<object> ExecuteDelegate { get; set; }
+
+    public bool CanExecute(object parameter)
+    {
+      var canExecute = this.CanExecuteDelegate;
+      return canExecute == null || canExecute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+      add { CommandManager.RequerySuggested += value; }
+      remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public void Execute(object parameter)
+    {
+      this.ExecuteDelegate?.Invoke(parameter);
+    }
+  }
+}
