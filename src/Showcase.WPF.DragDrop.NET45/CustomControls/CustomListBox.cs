b5cd53f867d010311e74d2706bc75a0295cb2447
Jan Karger <punkerat76@gmail.com>
2016-08-25 16:54:51 +0200

    Issue #30 sample

---
+++ src/Showcase.WPF.DragDrop.NET45/CustomControls/CustomListBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Showcase.WPF.DragDrop.CustomControls
+{
+  public class CustomListBox : ListBox
+  {
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+      return new CustomListBoxItem();
+    }
+
+    protected override bool IsItemItsOwnContainerOverride(object item)
+    {
+      return item is CustomListBoxItem;
+    }
+  }
+
+  public class CustomListBoxItem : ListBoxItem
+  {
+    public override string ToString()
+    {
+      return base.ToString();
+    }
+  }
+}
