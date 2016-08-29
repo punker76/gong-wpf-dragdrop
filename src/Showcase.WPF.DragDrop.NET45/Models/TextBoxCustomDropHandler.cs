939d79607a34dc6e64fe2082046df4a144abbebb
Jan Karger <jan.karger@inform-software.com>
2016-08-29 22:40:59 +0200

    Custom drop adorner sample for Issue #85

--- src/Showcase.WPF.DragDrop.NET45/Models/TextBoxCustomDropHandler.cs
+++ src/Showcase.WPF.DragDrop.NET45/Models/TextBoxCustomDropHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using GongSolutions.Wpf.DragDrop;
 
 namespace Showcase.WPF.DragDrop.Models
@@ -9,13 +10,44 @@ namespace Showcase.WPF.DragDrop.Models
   {
     public void DragOver(IDropInfo dropInfo)
     {
+      dropInfo.DropTargetAdorner = typeof(DropTargetHighlightAdorner);
       dropInfo.Effects = DragDropEffects.Move;
     }
 
     public void Drop(IDropInfo dropInfo)
     {
       var dataAsList = DefaultDropHandler.ExtractData(dropInfo.Data);
-      ((TextBox)dropInfo.VisualTarget).Text = string.Join(", ", dataAsList.OfType<object>().ToArray());
+      ((TextBox) dropInfo.VisualTarget).Text = string.Join(", ", dataAsList.OfType<object>().ToArray());
+    }
+  }
+
+  public class DropTargetHighlightAdorner : DropTargetAdorner
+  {
+    private readonly Pen _pen;
+    private readonly Brush _brush;
+
+    public DropTargetHighlightAdorner(UIElement adornedElement)
+      : base(adornedElement)
+    {
+      _pen = new Pen(Brushes.Tomato, 2);
+      _pen.Freeze();
+      _brush = new SolidColorBrush(Colors.Coral) {Opacity = 0.2};
+      this._brush.Freeze();
+
+      this.SetValue(SnapsToDevicePixelsProperty, true);
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+      var visualTarget = this.DropInfo.VisualTarget;
+      if (visualTarget != null)
+      {
+        var translatePoint = visualTarget.TranslatePoint(new Point(), this.AdornedElement);
+        translatePoint.Offset(1, 1);
+        var bounds = new Rect(translatePoint,
+          new Size(visualTarget.RenderSize.Width - 2, visualTarget.RenderSize.Height - 2));
+        drawingContext.DrawRectangle(_brush, _pen, bounds);
+      }
     }
   }
 }
