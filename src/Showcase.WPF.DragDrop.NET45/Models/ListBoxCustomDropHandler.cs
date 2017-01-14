f675d252d19c3567d26526fe4a916905fe584b43
Jan Karger <jan.karger@inform-software.com>
2017-01-15 00:16:14 +0100

    Add sample for #58

---
+++ src/Showcase.WPF.DragDrop.NET45/Models/ListBoxCustomDropHandler.cs
@@ -0,0 +1,31 @@
+﻿using GongSolutions.Wpf.DragDrop;
+
+namespace Showcase.WPF.DragDrop.Models
+{
+  public class ListBoxCustomDropHandler : DefaultDropHandler
+  {
+    public override void DragOver(IDropInfo dropInfo)
+    {
+      if (dropInfo.VisualTarget == dropInfo.DragInfo.VisualSource)
+      {
+        dropInfo.NotHandled = dropInfo.VisualTarget == dropInfo.DragInfo.VisualSource;
+      }
+      else
+      {
+        base.DragOver(dropInfo);
+      }
+    }
+
+    public override void Drop(IDropInfo dropInfo)
+    {
+      if (dropInfo.VisualTarget == dropInfo.DragInfo.VisualSource)
+      {
+        dropInfo.NotHandled = dropInfo.VisualTarget == dropInfo.DragInfo.VisualSource;
+      }
+      else
+      {
+        base.Drop(dropInfo);
+      }
+    }
+  }
+}
