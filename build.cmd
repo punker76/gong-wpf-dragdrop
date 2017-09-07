112f75dcb9a154f857f1923dc87bee7ba841cc1c
Jan Karger <punkerat76@gmail.com>
2017-09-07 22:18:07 +0200

    IF ELSE batching

--- build.cmd
+++ build.cmd
@@ -2,7 +2,6 @@
 tools\nuget\nuget.exe update -self
 tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion
 
-IF "%1" == "" tools\Cake\Cake.exe build.cake
-ELSE tools\Cake\Cake.exe build.cake --target=%1
+IF "%1" == "" ( tools\Cake\Cake.exe build.cake ) ELSE ( tools\Cake\Cake.exe build.cake --target=%1 )
 
 exit /b %errorlevel%
