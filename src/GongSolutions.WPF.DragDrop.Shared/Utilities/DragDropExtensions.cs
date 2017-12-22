2057a1ef2bf41425bb5652d7b62bf0fd4717b8fd
madben2000 <madben2000@users.noreply.github.com>
2017-12-22 13:59:19 +0100

    Creation of capture screen data template moved to extensions
    Functionality for external usage, e.g. data template selectors

--- src/GongSolutions.WPF.DragDrop.Shared/Utilities/DragDropExtensions.cs
+++ src/GongSolutions.WPF.DragDrop.Shared/Utilities/DragDropExtensions.cs
@@ -3,6 +3,10 @@ using System.Windows.Media;
 
 namespace GongSolutions.Wpf.DragDrop.Utilities
 {
+    using System;
+    using System.Windows.Controls;
+    using System.Windows.Media.Imaging;
+
     public static class DragDropExtensions
     {
         /// <summary>
@@ -50,6 +54,74 @@ namespace GongSolutions.Wpf.DragDrop.Utilities
             var relativeItemPosition = element.TranslatePoint(new Point(0, 0), relativeToElement);
             var relativeDropPosition = new Point(dropPosition.X - relativeItemPosition.X, dropPosition.Y - relativeItemPosition.Y);
             return VisualTreeHelper.GetDescendantBounds(element).Contains(relativeDropPosition); 
+        }
+
+        /// <summary>
+        /// Capture screen and create data template containing the captured image
+        /// </summary>
+        /// <param name="element">visual source to capture screen of</param>
+        /// <param name="visualSourceFlowDirection">Flowdirection of visual source</param>
+        /// <returns></returns>
+        public static DataTemplate GetCaptureScreenDataTemplate(this UIElement element, FlowDirection visualSourceFlowDirection)
+        {
+            DataTemplate template = null;
+            var bs = CaptureScreen(element, visualSourceFlowDirection);
+            if (bs != null)
+            {
+                var factory = new FrameworkElementFactory(typeof(Image));
+                factory.SetValue(Image.SourceProperty, bs);
+                factory.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+                factory.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                factory.SetValue(FrameworkElement.WidthProperty, bs.Width);
+                factory.SetValue(FrameworkElement.HeightProperty, bs.Height);
+                factory.SetValue(FrameworkElement.HorizontalAlignmentProperty, HorizontalAlignment.Left);
+                factory.SetValue(FrameworkElement.VerticalAlignmentProperty, VerticalAlignment.Top);
+                template = new DataTemplate { VisualTree = factory };
+            }
+            return template;
+        }
+
+        // Helper to generate the image - I grabbed this off Google 
+        // somewhere. -- Chris Bordeman cbordeman@gmail.com
+        private static BitmapSource CaptureScreen(Visual target, FlowDirection flowDirection)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            var dpiX = DpiHelper.DpiX;
+            var dpiY = DpiHelper.DpiY;
+
+            var bounds = VisualTreeHelper.GetDescendantBounds(target);
+            var dpiBounds = DpiHelper.LogicalRectToDevice(bounds);
+
+            var pixelWidth = (int)Math.Ceiling(dpiBounds.Width);
+            var pixelHeight = (int)Math.Ceiling(dpiBounds.Height);
+            if (pixelWidth < 0 || pixelHeight < 0)
+            {
+                return null;
+            }
+
+            var rtb = new RenderTargetBitmap(pixelWidth, pixelHeight, dpiX, dpiY, PixelFormats.Pbgra32);
+
+            var dv = new DrawingVisual();
+            using (var ctx = dv.RenderOpen())
+            {
+                var vb = new VisualBrush(target);
+                if (flowDirection == FlowDirection.RightToLeft)
+                {
+                    var transformGroup = new TransformGroup();
+                    transformGroup.Children.Add(new ScaleTransform(-1, 1));
+                    transformGroup.Children.Add(new TranslateTransform(bounds.Size.Width - 1, 0));
+                    ctx.PushTransform(transformGroup);
+                }
+                ctx.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+            }
+
+            rtb.Render(dv);
+
+            return rtb;
         }
     }
 }
