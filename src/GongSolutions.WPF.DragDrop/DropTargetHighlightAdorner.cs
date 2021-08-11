5f29a7d394389928adc4cb6c82d19b93e69ee82b
Metritutus <flying_saucers@hotmail.co.uk>
2021-08-11 22:47:49 +0100

    Added bounds calculation workaround
    * Added VisualTreeDescendantBoundsHelper to work around bug/feature of VisualTreeHelper.GetDescendantBounds() including collapsed Visuals in its bounds calculations.

--- src/GongSolutions.WPF.DragDrop/DropTargetHighlightAdorner.cs
+++ src/GongSolutions.WPF.DragDrop/DropTargetHighlightAdorner.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using GongSolutions.Wpf.DragDrop.Utilities;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -28,7 +29,7 @@ namespace GongSolutions.Wpf.DragDrop
                 var tvItem = visualTargetItem as TreeViewItem;
                 if (tvItem != null && VisualTreeHelper.GetChildrenCount(tvItem) > 0)
                 {
-                    var descendant = VisualTreeHelper.GetDescendantBounds(tvItem);
+                    var descendant = VisualTreeExtensions.GetVisibleDescendantBounds(tvItem);
                     var translatePoint = tvItem.TranslatePoint(new Point(), this.AdornedElement);
                     var itemRect = new Rect(translatePoint, tvItem.RenderSize);
                     descendant.Union(itemRect);
@@ -37,7 +38,7 @@ namespace GongSolutions.Wpf.DragDrop
                 }
                 if (rect.IsEmpty)
                 {
-                    rect = new Rect(visualTargetItem.TranslatePoint(new Point(), this.AdornedElement), VisualTreeHelper.GetDescendantBounds(visualTargetItem).Size);
+                    rect = new Rect(visualTargetItem.TranslatePoint(new Point(), this.AdornedElement), VisualTreeExtensions.GetVisibleDescendantBounds(visualTargetItem).Size);
                 }
                 drawingContext.DrawRoundedRectangle(null, this.Pen, rect, 2, 2);
             }
