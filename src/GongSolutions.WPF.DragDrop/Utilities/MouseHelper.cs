f6fed7ee794f6151bc58b05d022414ffa6d15ee4
punker76 <punkerat76@gmail.com>
2022-09-03 23:36:01 +0200

    fix: #418 handle GetCursorPos failed calles

--- src/GongSolutions.WPF.DragDrop/Utilities/MouseHelper.cs
+++ src/GongSolutions.WPF.DragDrop/Utilities/MouseHelper.cs
@@ -11,56 +11,50 @@ namespace GongSolutions.Wpf.DragDrop.Utilities
 
         private const int WH_MOUSE_LL = 14;
 
-        private static bool _TimerTriggerd;
-        private static IntPtr _hookID = IntPtr.Zero;
+        private static bool _timerTriggered;
         private static Action<System.Windows.Point> _mouseMoveHandler;
 
         // wee need to keep the variable to prevent the GarbageCollector to remove the HookCallback
         // https://social.msdn.microsoft.com/Forums/vstudio/en-US/68fdc3dc-8d77-48c4-875c-5312baa56aee/how-to-fix-callbackoncollecteddelegate-exception?forum=netfxbcl
         private static LowLevelMouseProc _proc = HookCallback;
-                private static System.Windows.Threading.DispatcherTimer _timer;
+        private static System.Windows.Threading.DispatcherTimer _timer;
 
-        internal static IntPtr HookMouseMove(bool TimerTriggerd, Action<System.Windows.Point> mouseMoveHandler)
+        internal static IntPtr HookMouseMove(bool timerTriggered, Action<System.Windows.Point> mouseMoveHandler)
         {
             _mouseMoveHandler = mouseMoveHandler;
-            _TimerTriggerd = TimerTriggerd;
+            _timerTriggered = timerTriggered;
 
-            if (_TimerTriggerd)
+            if (_timerTriggered)
             {
                 _timer = new System.Windows.Threading.DispatcherTimer(System.Windows.Threading.DispatcherPriority.Input);
-                _timer.Tick += (sender, args) =>
-                {
-                    POINT lpPoint;
-                    if (GetCursorPos(out lpPoint))
-                        _mouseMoveHandler?.Invoke(new System.Windows.Point(lpPoint.x, lpPoint.y));
-                };
+                _timer.Tick += (_, _) =>
+                    {
+                        if (TryGetCursorPos(out var lpPoint))
+                        {
+                            _mouseMoveHandler?.Invoke(new System.Windows.Point(lpPoint.x, lpPoint.y));
+                        }
+                    };
                 _timer.Interval = new TimeSpan(1);
                 _timer.Start();
 
                 return IntPtr.Zero;
             }
-            else
-            {
-                using (var process = Process.GetCurrentProcess())
-                {
-                    using (var module = process.MainModule)
-                    {
-                        return SetWindowsHookEx(WH_MOUSE_LL, _proc, GetModuleHandle(module.ModuleName), 0);
-                    }
-                }
-            }
+
+            using var process = Process.GetCurrentProcess();
+            using var module = process.MainModule;
+            return module != null ? SetWindowsHookEx(WH_MOUSE_LL, _proc, GetModuleHandle(module.ModuleName), 0) : IntPtr.Zero;
         }
 
         internal static void RemoveHook(IntPtr hookId)
         {
-            if (_TimerTriggerd)
+            if (_timerTriggered)
             {
                 _timer.Stop();
             }
-            else 
+            else
             {
                 UnhookWindowsHookEx(hookId);
-            }            
+            }
         }
 
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
@@ -68,12 +62,11 @@ namespace GongSolutions.Wpf.DragDrop.Utilities
             if (nCode >= 0 && MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
             {
                 MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                //Debug.WriteLine(hookStruct.pt.x + ", " + hookStruct.pt.y);
                 var mousePosScreen = new System.Windows.Point(hookStruct.pt.x, hookStruct.pt.y);
                 _mouseMoveHandler?.Invoke(mousePosScreen);
             }
 
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
         }
 
         private enum MouseMessages
@@ -115,8 +108,25 @@ namespace GongSolutions.Wpf.DragDrop.Utilities
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
-        
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll", EntryPoint = "GetCursorPos", ExactSpelling = true, CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool _GetCursorPos(out POINT lpPoint);
+
+        private static bool TryGetCursorPos(out POINT pt)
+        {
+            var returnValue = _GetCursorPos(out pt);
+            // Sometimes Win32 will fail this call, such as if you are
+            // not running in the interactive desktop. For example,
+            // a secure screen saver may be running.
+            if (!returnValue)
+            {
+                Trace.WriteLine("GetCursorPos failed!");
+                pt.x = 0;
+                pt.y = 0;
+            }
+
+            return returnValue;
+        }
     }
 }
