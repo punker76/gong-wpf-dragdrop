6615d45ac71cb4f17d54081088d87b8ee23f1d12
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2024-03-26 15:01:45 +0100

    Reverted some unintended changes to DropInfo constructor

--- src/GongSolutions.WPF.DragDrop/DropInfo.cs
+++ src/GongSolutions.WPF.DragDrop/DropInfo.cs
@@ -188,11 +188,10 @@ namespace GongSolutions.Wpf.DragDrop
         {
             this.eventArgs = e;
             this.DragInfo = dragInfo;
-            this.KeyStates = e?.KeyStates ?? DragDropKeyStates.None;
+            this.KeyStates = e.KeyStates;
             this.EventType = eventType;
-            var dataFormat = dragInfo?.DataFormat ?? dragInfo?.DataFormat;
-            if(e?.Data != null)
-                this.Data = dataFormat != null && e.Data.GetDataPresent(dataFormat.Name) ? e.Data.GetData(dataFormat.Name) : dragInfo?.Data;
+            var dataFormat = dragInfo?.DataFormat;
+            this.Data = dataFormat != null && e.Data.GetDataPresent(dataFormat.Name) ? e.Data.GetData(dataFormat.Name) : e.Data;
 
             this.VisualTarget = sender as UIElement;
             // if there is no drop target, find another
@@ -225,7 +224,7 @@ namespace GongSolutions.Wpf.DragDrop
             this.TargetScrollingMode = this.VisualTarget != null ? DragDrop.GetDropScrollingMode(this.VisualTarget) : ScrollingMode.Both;
 
             // visual target can be null, so give us a point...
-            this.DropPosition = this.VisualTarget != null && e != null ? e.GetPosition(this.VisualTarget) : new Point();
+            this.DropPosition = this.VisualTarget != null ? e.GetPosition(this.VisualTarget) : new Point();
 
             this.Update();
         }
