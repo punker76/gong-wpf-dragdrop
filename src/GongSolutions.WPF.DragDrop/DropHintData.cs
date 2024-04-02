8a3f61da2eb7a4278cd4d4f0387da49d9685a526
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2024-04-02 09:51:15 +0200

    Implemented INotifyPropertyChanged on DropHintData to avoid WPF binding leak

--- src/GongSolutions.WPF.DragDrop/DropHintData.cs
+++ src/GongSolutions.WPF.DragDrop/DropHintData.cs
@@ -1,10 +1,17 @@
 ﻿namespace GongSolutions.Wpf.DragDrop;
 
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
 /// <summary>
 /// Data presented in drop hint adorner.
 /// </summary>
-public class DropHintData
+public class DropHintData : INotifyPropertyChanged
 {
+    private DropHintState hintState;
+    private string hintText;
+
     public DropHintData(DropHintState hintState, string hintText)
     {
         this.HintState = hintState;
@@ -15,10 +22,37 @@ public class DropHintData
     /// The hint text to display to the user. See <see cref="IDropInfo.DropHintText"/>
     /// and <see cref="IDropHintInfo.DropHintText"/>.
     /// </summary>
-    public string HintText { get; set; }
+    public string HintText
+    {
+        get => this.hintText;
+        set
+        {
+            if (value == this.hintText) return;
+            this.hintText = value;
+            this.OnPropertyChanged();
+        }
+    }
+
     /// <summary>
     /// The hint state to display different colors for hints. See <see cref="IDropInfo.DropTargetHintState"/>
     /// and <see cref="IDropHintInfo.DropTargetHintState"/>.
     /// </summary>
-    public DropHintState HintState { get; set; }
+    public DropHintState HintState
+    {
+        get => this.hintState;
+        set
+        {
+            if (value == this.hintState) return;
+            this.hintState = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
